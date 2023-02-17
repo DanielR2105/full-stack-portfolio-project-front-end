53535a0aab3394d430e6b9d56770c9624c00aa94
DanielR2105 <rodriguez.daniel2105@gmail.com>
2023-02-17 00:31:01 -0500

    fixed error in show club

--- src/pages/Showbookclub.js
+++ src/pages/Showbookclub.js
@@ -1,6 +1,6 @@
 import BookclubDetails from "../components/BookclubDetails";
 
-export default function Showbookclub() {
+export default function ShowBookclub() {
   return (
     <div>
       <BookclubDetails />
